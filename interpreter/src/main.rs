8b079082c203e12cc3838e9700bff7a9e02b1acc
Russell Mirabelli <russell@mirabellifamily.org>
2026-03-23 22:01:22 -0500

    in class

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,6 +1,7 @@
 #[derive(Copy,Clone)]
 enum Primitive {
     Add,
+    Subtract,
     Multiply,
     Number(i32)
 }
@@ -11,6 +12,7 @@ fn evaluate(primitives: Vec<Primitive>) -> i32 {
     iter.next();
     match firstElement {
         Primitive::Add => {iter.fold(0, |t,n| t + evaluate(vec![*n]))},
+        Primitive::Subtract  => {let a = evaluate(vec![*iter.next().unwrap()]);iter.fold(a, |t,n| t - evaluate(vec![*n]))},
         Primitive::Multiply => {iter.fold(1,|p,n| p * evaluate(vec![*n])) },
         Primitive::Number(val) => *val
     } 
@@ -19,8 +21,8 @@ fn evaluate(primitives: Vec<Primitive>) -> i32 {
 fn main() {
 
     let mut primitives = Vec::<Primitive>::new();
-    primitives.push(Primitive::Multiply);
-    primitives.push(Primitive::Number(3));
+    primitives.push(Primitive::Subtract);
+    primitives.push(Primitive::Number(10));
     primitives.push(Primitive::Number(4));
     primitives.push(Primitive::Number(5));
     let result = evaluate(primitives);
