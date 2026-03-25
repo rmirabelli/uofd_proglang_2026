ad3f976dde45ab8bfc79a477c83ee592f4be90c2
Russell Mirabelli <russell@mirabellifamily.org>
2026-03-25 07:25:09 -0500

    in class

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,31 +1,100 @@
-#[derive(Copy,Clone)]
-enum Primitive {
-    Add,
-    Subtract,
-    Multiply,
+#[derive(Clone)]
+#[derive(Debug)]
+pub enum Expression {
+    Add(Vec<Expression>),
+    Subtract(Vec<Expression>),
+    Multiply(Vec<Expression>),
     Number(i32)
 }
 
-fn evaluate(primitives: Vec<Primitive>) -> i32 {
-    let firstElement = &primitives[0];
-    let mut iter = primitives.iter();
-    iter.next();
-    match firstElement {
-        Primitive::Add => {iter.fold(0, |t,n| t + evaluate(vec![*n]))},
-        Primitive::Subtract  => {let a = evaluate(vec![*iter.next().unwrap()]);iter.fold(a, |t,n| t - evaluate(vec![*n]))},
-        Primitive::Multiply => {iter.fold(1,|p,n| p * evaluate(vec![*n])) },
-        Primitive::Number(val) => *val
-    } 
+pub fn evaluate_addition(add: &Expression) -> i32 {
+    if let Expression::Add(expressions) = add {
+        let iter = expressions.iter();
+        iter.fold(0, |total, next| total + evaluate(next))
+    } else {
+        panic!("Addition not provided");
+    }
+}
+
+pub fn evaluate_multiplication(mult: &Expression) -> i32 {
+    if let Expression::Multiply(expressions) = mult {
+        let iter = expressions.iter();
+        iter.fold(1,|total, next| total * evaluate(next))
+    } else {
+        panic!("Multiply not provided");
+    }
+}
+
+pub fn evaluate_subtraction(sub: &Expression) -> i32 {
+    if let Expression::Subtract(expressions) = sub {
+            let mut iter = expressions.iter();
+            let first = iter.next().unwrap();
+            iter.fold(evaluate(first), |total, next| total - evaluate(next))
+    } else {
+        panic!("Not subtraction");
+    }
+}
+
+fn evaluate(expression: &Expression) -> i32 {
+    match expression {
+        Expression::Add(_) => evaluate_addition(expression),
+        Expression::Subtract(_) => evaluate_subtraction(expression),
+        Expression::Multiply(_) => evaluate_multiplication(expression),
+        Expression::Number(val) => *val,
+    }
 }
 
 fn main() {
-
-    let mut primitives = Vec::<Primitive>::new();
-    primitives.push(Primitive::Subtract);
-    primitives.push(Primitive::Number(10));
-    primitives.push(Primitive::Number(4));
-    primitives.push(Primitive::Number(5));
-    let result = evaluate(primitives);
+    let mut expressions = Vec::new();
+    expressions.push(Expression::Number(3));
+    expressions.push(Expression::Number(4));
+    expressions.push(Expression::Number(5));
+    let add = Expression::Add(expressions);
+    let multiply = Expression::Multiply(vec![add, Expression::Number(2)]);
+    let result = evaluate(&multiply);
     println!("The result is {result}");
 
 }
+
+#[cfg(test)]
+mod tests {
+use crate::{evaluate_addition, evaluate_subtraction, Expression};
+    #[test]
+    fn it_works() {
+        assert_eq!(2+2, 4);
+    }
+
+    #[test]
+    fn test_basic_addition() {
+        // arrange
+        let values = vec![Expression::Number(2), Expression::Number(2)];
+        // act
+        let sum = evaluate_addition(&Expression::Add(values));
+        // assert
+        assert_eq!(sum, 4);
+    }
+
+    #[test]
+    fn test_basic_subtraction() {
+        // arrange
+        let values = vec![Expression::Number(2), Expression::Number(2)];
+        // act
+        let difference = evaluate_subtraction(&Expression::Subtract(values));
+        // assert
+        assert_eq!(difference, 0);
+    }
+    #[test]
+    fn test_basic_addition_not5() {
+        // arrange
+        let values = vec![Expression::Number(2), Expression::Number(2)];
+        // act
+        let sum = evaluate_addition(&Expression::Add(values));
+        // assert
+        assert_ne!(sum, 5);
+    }
+
+}
+
+
+
+
